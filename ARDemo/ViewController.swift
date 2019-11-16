754304ac92ef00fbe3363803073f0895505b62a9
Steven Lipton <shlomo626@gmail.com>
2019-11-16 07:50:48 -0600

    initial commit

--- ARDemo/ViewController.swift
+++ ARDemo/ViewController.swift
@@ -17,9 +17,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
+        //let boxAnchor = try! Experience.loadBox()
+        let myAnchor = try! BallBlock.loadScene()
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(myAnchor)
     }
 }
